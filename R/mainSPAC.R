dcc5623d110a161ded92724dd51bee0f3eda7eec
micau80 <56366785+micau80@users.noreply.github.com>
2019-10-30 13:52:32 -0400

    Update mainSPAC.R

--- R/mainSPAC.R
+++ R/mainSPAC.R
@@ -71,7 +71,8 @@ SPAC <- function(y1=NULL, y2=NULL,
                    G=NULL,
                    covariates = NULL,
                    prev = 0.1,
-                   prev2 = 0.06112,
+                   pi.un = NULL,
+                   pi.af =NULL,
                    cutoffs = NULL,
                    link = "probit",
                    copfit = "Gaussian",
@@ -96,7 +97,7 @@ SPAC <- function(y1=NULL, y2=NULL,
     switch(Design,
          MT={
            outfit = gwas_cop_mt_snps(y1=y1, y2=y2, marker=G, covar=covariates, y2ub=cutoffs, link=link, cop=copfit,
-                    lik=method, prev = prev, prev2 =prev2) # I have to add prev to this function
+                    lik=method, pi.un = pi.un, pi.af = pi.af) # I have to add prev to this function
            
            results <- list(intercept.SNP.SecP = c(outfit$b02, outfit$se02),
                            SNP.SecP = c(outfit$b12,outfit$se2), 
